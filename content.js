9dad202b4f7a641e3e4965c6780150af5f760398
Jocelyn10 <jocelynnsa@gmail.com>
2022-07-12 12:37:00 +0100

    feature: change all h2 atgs

--- content.js
+++ content.js
@@ -14,3 +14,9 @@ for (let i = 0; i < imgs.length; i++) {
   const randomImg = Math.floor(Math.random() * willSmithImages.length);
   imgs[i].src = willSmithImages[randomImg];
 }
+
+const headers = document.getElementsByTagName('h2');
+
+for (let i = 0; i < headers.length; i++) {
+  headers[i].innerText = 'Jocelyn is awesome';
+}
