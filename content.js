9d8bec310d0008f14193731e99493e904f75fd11
Jocelyn10 <jocelynnsa@gmail.com>
2022-07-12 11:58:33 +0100

    create pictures folder

---
+++ content.js
@@ -0,0 +1,9 @@
+let willSmithImages = [
+  '/pictures/will1.jpg',
+  '/pictures/will5.jpg',
+  '/pictures/will3.jpg',
+  '/pictures/will4.jpg',
+  '/pictures/will5.jpg',
+  '/pictures/will6.jpg',
+  '/pictures/will7.jpg',
+];
